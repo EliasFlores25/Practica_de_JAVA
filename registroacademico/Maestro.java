3ea367abd7da132d99b91c070af5b68e936474fc
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-05-21 16:05:32 -0600

    SEGUNDOS CAMBIOS

--- registroacademico/Maestro.java
+++ registroacademico/Maestro.java
@@ -1,5 +1,32 @@
 package registroacademico;
 
-public class Maestro {
+public class Maestro extends DatoGeneral{
+    private String dui;
+    private String grados;
+    public Maestro() {
+    }
+    public Maestro(String nombre, String direccion, String telefono, String email, String dui, String grados) {
+        super(nombre, direccion, telefono, email);
+        this.dui = dui;
+        this.grados = grados;
+    }
+    public String getDui() {
+        return dui;
+    }
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+    public String getGrados() {
+        return grados;
+    }
+    public void setGrados(String grados) {
+        this.grados = grados;
+    }
+    @Override
+    public void modificar() {
+      System.out.println("Modificando Maestro");
+      System.out.println("DUI: " + dui);
+      System.out.println("Grados: " + grados);
+    }
 
 }
