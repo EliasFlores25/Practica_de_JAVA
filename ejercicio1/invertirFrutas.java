c55963fde8933c2a356e2e0bb62658a085df7f46
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-05-25 07:28:22 -0600

    Avances de ejercicio

--- ejercicio1/invertirFrutas.java
+++ ejercicio1/invertirFrutas.java
@@ -1,5 +1,26 @@
 package ejercicio1;
+import java.util.Scanner;
 
 public class invertirFrutas {
 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        String[] frutas = new String[10];
+
+        System.out.println("Por favor ingrese 10 nombres de frutas:");
+
+        for (int i = 0; i < frutas.length; i++) {
+            System.out.println("Ingrese el nombre de la fruta " + (i + 1) + ": ");
+            frutas[i] = entrada.nextLine();
+        }
+
+        System.out.println("Se muestra el contenido del vector 'FRUTAS' en orden inverso:");
+
+        for (int i = frutas.length - 1; i >= 0; i--) {
+            System.out.println("Fruta " + (i + 1) + ": " + frutas[i]);
+        }
+
+        entrada.close();
+    }
 }
