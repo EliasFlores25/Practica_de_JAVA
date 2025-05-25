ae8e54f4fbde975bada0e4a64df178bce478a1e8
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-05-25 10:40:00 -0600

    Modificacion ejercicio1

--- ejercicio1/invertirFrutas.java
+++ ejercicio1/invertirFrutas.java
@@ -2,23 +2,21 @@ package ejercicio1;
 import java.util.Scanner;
 
 public class invertirFrutas {
-
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-
         String[] frutas = new String[10];
 
-        System.out.println("Por favor ingrese 10 nombres de frutas:");
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.println("Ingrese 10 nombres de frutas:");
         for (int i = 0; i < frutas.length; i++) {
-            System.out.println("Ingrese el nombre de la fruta " + (i + 1) + ": ");
+            System.out.print("Fruta " + (i + 1) + ": ");
             frutas[i] = entrada.nextLine();
         }
 
-        System.out.println("Se muestra el contenido del vector 'FRUTAS' en orden inverso:");
-
+        System.out.println("
+        Contenido del vector en forma inversa:");
         for (int i = frutas.length - 1; i >= 0; i--) {
-            System.out.println("Fruta " + (i + 1) + ": " + frutas[i]);
+            System.out.println(frutas[i]);
         }
 
         entrada.close();
