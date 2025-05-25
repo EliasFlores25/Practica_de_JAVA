96c3f05b9e278ed6d2a7828b1067b19b3b9c71d5
Elisandro Elias Flores Zaña <EliasEFloresZ2006@outlook.com>
2025-05-25 10:42:26 -0600

    Cambios en ejercicio2

--- ejercicio2/promedioVector.java
+++ ejercicio2/promedioVector.java
@@ -1,33 +1,20 @@
 import java.util.Scanner;
 
 public class promedioVector {
-
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-
         int[] numeros = new int[12];
+        Scanner entrada = new Scanner(System.in);
         int suma = 0;
 
-        System.out.println("Por favor, ingrese 12 números enteros:");
-
+        System.out.println("Ingrese 12 números enteros:");
         for (int i = 0; i < numeros.length; i++) {
-            System.out.print("Ingrese el número #" + (i + 1) + ": ");
+            System.out.print("Número " + (i + 1) + ": ");
             numeros[i] = entrada.nextInt();
             suma += numeros[i];
         }
 
-        
         double promedio = (double) suma / numeros.length;
-
-        System.out.println("Números ingresados:");
-       
-        for (int i = 0; i < numeros.length; i++) {
-            System.out.print(numeros[i] + (i == numeros.length - 1 ? "" : ", "));
-        }
-        System.out.println();
-
-        System.out.println("La suma de los números es: " + suma);
-        System.out.println("El promedio de los números ingresados es: " + String.format("%.2f", promedio));
+        System.out.println("El promedio de los números ingresados es: " + promedio);
 
         entrada.close();
     }
